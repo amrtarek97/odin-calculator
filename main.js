da4e3dc2389f6f3923c6d290849a5835a441a84c
amrtarek97 <121955814+amrtarek97@users.noreply.github.com>
2024-05-16 16:20:36 +0300

    finish html and css

--- main.js
+++ main.js
@@ -31,3 +31,25 @@ function divide(a,b){
 // second thing 
 
 
+
+// grab the display
+
+const display = document.querySelector("#display");
+
+display.value = "0"
+
+//grabbing all buttons
+
+const oneBtn = document.querySelector("#no1");
+const twoBtn = document.querySelector("#no2"); 
+const threeBtn = document.querySelector("#no3"); 
+const fourBtn = document.querySelector("#no4"); 
+const fiveBtn = document.querySelector("#no5"); 
+const sixBtn = document.querySelector("#no6"); 
+const sevenBtn = document.querySelector("#no7"); 
+const eightBtn = document.querySelector("#no8"); 
+const nineBtn = document.querySelector("#no9");
+
+oneBtn.addEventListener("onclick", changeDisplay());
+
+function changeDisplay
