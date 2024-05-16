a5719270523cba2a2dc927acda2a9fe704d7a915
amrtarek97 <121955814+amrtarek97@users.noreply.github.com>
2024-05-16 15:19:27 +0300

    create main files

---
+++ main.js
@@ -0,0 +1,33 @@
+// opeartion functions
+
+//1- add
+
+function add(a,b){
+    return a+b;
+}
+
+//2- subtract 
+
+function sub(a,b){
+    return a-b;
+}
+
+//3- multiply
+
+function multiply(a,b){
+    return a*b;
+}
+
+//4- divide
+
+function divide(a,b){
+    if (b === 0){
+        return "nah bruh";
+    } else{
+        return a/b;
+    }
+}
+
+// second thing 
+
+
